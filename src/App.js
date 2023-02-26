8a7a4ac9b28f68f6eb3628214978459947b9684c
Patryk <krasowski.p@outlook.com>
2023-02-26 20:58:16 +0100

    App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NewUser from './components/NewUsers/NewUser'
+import CurrentUsers from './components/CurrentUsers/CurrentUsers'
+
+function App() {
+	return (
+		<div>
+			<NewUser />
+			<CurrentUsers />
+		</div>
+	)
+}
+
+export default App
