820bae80039993982064831c49c1d10e783c7238
Patryk <krasowski.p@outlook.com>
2023-02-27 18:42:16 +0100

    added dummy array with users, useState, function which get new user and add to users array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NewUser from './components/NewUsers/NewUser'
 import CurrentUsers from './components/CurrentUsers/CurrentUsers'
 
 function App() {
+	const DUMMY_USERS = [
+		{ key: Math.random() * 1000, name: 'Patryk', age: 28 },
+		{ key: Math.random() * 1000, name: 'Aleksandra', age: 30 },
+		{ key: Math.random() * 1000, name: 'Kacper', age: 3 },
+	]
+
+	const [users, setUsers] = useState(DUMMY_USERS)
+
+	const getEnteredUser = enteredUser => {
+		const newUser = enteredUser
+		setUsers(prevDUMMY_USERS => {
+			return [enteredUser, ...prevDUMMY_USERS]
+		})
+	}
+
 	return (
 		<div>
-			<NewUser />
-			<CurrentUsers />
+			<NewUser onGetEnteredUser={getEnteredUser} />
+			<CurrentUsers users={users} />
 		</div>
 	)
 }
