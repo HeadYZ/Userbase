131e184ebb83a80e249f024f58dfdf4ee2bff8cb
Patryk <krasowski.p@outlook.com>
2023-03-01 22:02:04 +0100

    deleted Dummy_Users array, useState change on empty array

--- src/App.js
+++ src/App.js
@@ -3,18 +3,11 @@ import NewUser from './components/NewUsers/NewUser'
 import CurrentUsers from './components/CurrentUsers/CurrentUsers'
 
 function App() {
-	const DUMMY_USERS = [
-		{ key: Math.random() * 1000, name: 'Patryk', age: 28 },
-		{ key: Math.random() * 1000, name: 'Aleksandra', age: 30 },
-		{ key: Math.random() * 1000, name: 'Kacper', age: 3 },
-	]
-
-	const [users, setUsers] = useState(DUMMY_USERS)
+	const [users, setUsers] = useState([])
 
 	const getEnteredUser = enteredUser => {
-		const newUser = enteredUser
-		setUsers(prevDUMMY_USERS => {
-			return [enteredUser, ...prevDUMMY_USERS]
+		setUsers(prevUsers => {
+			return [...prevUsers, enteredUser]
 		})
 	}
 
