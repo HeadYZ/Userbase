b98d4eef77c49f4931f43a4bfca52acbfe315d5f
Patryk <krasowski.p@outlook.com>
2023-03-01 22:11:15 +0100

    deleted currentUser component transferring its value to CurrentUsers component

--- src/components/CurrentUsers/CurrentUsers.js
+++ src/components/CurrentUsers/CurrentUsers.js
@@ -1,13 +1,14 @@
-import styles from './CurrentUsers.module.css'
+import classes from './CurrentUsers.module.css'
 import Card from '../UI/Card'
-import CurrentUser from './CurrentUser'
 
 const CurrentUsers = props => {
 	return (
 		<Card>
 			<ul>
 				{props.users.map(user => (
-					<CurrentUser key={user.key} name={user.name} age={user.age} />
+					<li className={classes['user-list__item']} key={user.key}>
+						{user.name} ({user.age} years old)
+					</li>
 				))}
 			</ul>
 		</Card>
