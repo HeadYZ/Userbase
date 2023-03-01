086c16a43ff9074d90d8376fe4571bdf27d25576
Patryk <krasowski.p@outlook.com>
2023-03-01 22:04:22 +0100

    created reusable errorModal component which display error info

---
+++ src/components/UI/ErrorModal.js
@@ -0,0 +1,22 @@
+import Card from './Card'
+import Button from './Button'
+import classes from './ErrorModal.module.css'
+
+const ErrorModal = props => {
+	return (
+        <div>
+            <div className={classes.modal__backdrop} />
+		<Card className={classes.modal}>
+			<div>
+				<h2 className={classes.modal__heading}>{props.title}</h2>
+				<div className={classes.modal__content}>
+					<p>{props.information}.</p>
+					<Button className={classes.modal__button} onClick={props.onConfirm}>Okay</Button>
+				</div>
+			</div>
+		</Card>
+        </div>
+	)
+}
+
+export default ErrorModal
