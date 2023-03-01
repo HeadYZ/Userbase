1afb8358aff323db7ecb263032b696dc6225aa90
Patryk <krasowski.p@outlook.com>
2023-03-01 22:09:17 +0100

    added errorModal component and in the onSubmitHandler function added a condition to check if the data in the form was completed correctly

--- src/components/NewUsers/NewUser.js
+++ src/components/NewUsers/NewUser.js
@@ -1,10 +1,16 @@
-import styles from './NewUser.module.css'
 import { useState } from 'react'
+import classes from './NewUser.module.css'
+import ErrorModal from '../UI/ErrorModal'
+import Button from '../UI/Button'
 import Card from '../UI/Card'
 
 const NewUser = props => {
 	const [enteredName, setEnteredName] = useState('')
 	const [enteredAge, setEnteredAge] = useState('')
+
+	const [isValid, setIsValid] = useState(false)
+
+	const [text, setText] = useState('')
 
 	const usernameChangeHandler = e => {
 		setEnteredName(e.target.value)
@@ -16,26 +22,51 @@ const NewUser = props => {
 	const onSubmitHandler = e => {
 		e.preventDefault()
 
-		const userData = {
-			key: Math.random() * 1000,
-			name: enteredName,
-			age: enteredAge,
+		if (enteredAge.trim().length === 0 || enteredName.trim().length === 0) {
+			setIsValid(true)
+			setText('Please enter a valid name and age (non-empty values)')
+		} else if (+enteredAge < 0) {
+			setIsValid(true)
+			setText('Please enter a valid age (> 0)')
+		} else {
+			const userData = {
+				key: Math.random() * 1000,
+				name: enteredName,
+				age: enteredAge,
+			}
+
+			props.onGetEnteredUser(userData)
+
+			setEnteredName('')
+			setEnteredAge('')
 		}
+	}
 
-		props.onGetEnteredUser(userData)
-
-		setEnteredName('')
-		setEnteredAge('')
+	const onConfirmHandler = () => {
+		setIsValid(false)
 	}
 
 	return (
 		<Card>
-			<form onSubmit={onSubmitHandler} className={styles.form}>
-				<label className={styles.form__heading}>Username</label>
-				<input className={styles.form__input} type='text' onChange={usernameChangeHandler} value={enteredName}></input>
-				<label className={styles.form__heading}>Age (Years)</label>
-				<input className={styles.form__input} type='number' onChange={ageChangeHandler} value={enteredAge}></input>
-				<button className={styles.form__button}>Add User</button>
+			<form onSubmit={onSubmitHandler} className={classes.form}>
+				<label className={classes.form__heading}>Username</label>
+				<input className={classes.form__input} type='text' onChange={usernameChangeHandler} value={enteredName}></input>
+				<label className={classes.form__heading}>Age (Years)</label>
+				<input className={classes.form__input} type='number' onChange={ageChangeHandler} value={enteredAge}></input>
+				<Button type='submit'>
+					Add User
+				</Button>
+
+				{isValid ? (
+					<ErrorModal
+						isvalid={isValid}
+						onConfirm={onConfirmHandler}
+						title={'Invalid input'}
+						information={text}
+					/>
+				) : (
+					''
+				)}
 			</form>
 		</Card>
 	)
