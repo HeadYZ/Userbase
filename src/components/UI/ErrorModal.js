88b39157231a095ee8f364a22da950297e676c33
Patryk <krasowski.p@outlook.com>
2023-03-03 19:31:24 +0100

    change to a reusable component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -4,18 +4,20 @@ import classes from './ErrorModal.module.css'
 
 const ErrorModal = props => {
 	return (
-        <div>
-            <div className={classes.modal__backdrop} />
-		<Card className={classes.modal}>
-			<div>
-				<h2 className={classes.modal__heading}>{props.title}</h2>
-				<div className={classes.modal__content}>
-					<p>{props.information}.</p>
-					<Button className={classes.modal__button} onClick={props.onConfirm}>Okay</Button>
+		<div>
+			<div className={classes.modal__backdrop} onClick={props.onConfirm} />
+			<Card className={classes.modal}>
+				<div>
+					<h2 className={classes.modal__heading}>{props.title}</h2>
+					<div className={classes.modal__content}>
+						<p>{props.information}.</p>
+						<Button className={classes.modal__button} onClick={props.onConfirm}>
+							Okay
+						</Button>
+					</div>
 				</div>
-			</div>
-		</Card>
-        </div>
+			</Card>
+		</div>
 	)
 }
 
