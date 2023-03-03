44fbc9732524b024d8249510dfb811c77d4a4052
Patryk <krasowski.p@outlook.com>
2023-03-03 19:33:24 +0100

    chanded isValid state to error state, changed if in onSumbitHandler function

--- src/components/NewUsers/NewUser.js
+++ src/components/NewUsers/NewUser.js
@@ -8,9 +8,7 @@ const NewUser = props => {
 	const [enteredName, setEnteredName] = useState('')
 	const [enteredAge, setEnteredAge] = useState('')
 
-	const [isValid, setIsValid] = useState(false)
-
-	const [text, setText] = useState('')
+	const [error, setError] = useState()
 
 	const usernameChangeHandler = e => {
 		setEnteredName(e.target.value)
@@ -23,11 +21,15 @@ const NewUser = props => {
 		e.preventDefault()
 
 		if (enteredAge.trim().length === 0 || enteredName.trim().length === 0) {
-			setIsValid(true)
-			setText('Please enter a valid name and age (non-empty values)')
-		} else if (+enteredAge < 0) {
-			setIsValid(true)
-			setText('Please enter a valid age (> 0)')
+			setError({
+				title: 'Invalid input',
+				information: 'Please enter a valid name and age (non-empty values)',
+			})
+		} else if (+enteredAge < 1) {
+			setError({
+				title: 'Invalid age',
+				information: 'Please enter a valid age (> 0)',
+			})
 		} else {
 			const userData = {
 				key: Math.random() * 1000,
@@ -43,30 +45,24 @@ const NewUser = props => {
 	}
 
 	const onConfirmHandler = () => {
-		setIsValid(false)
+		setError(null)
 	}
 
 	return (
 		<Card>
+			{error && (
+				<ErrorModal
+					onConfirm={onConfirmHandler}
+					title={error.title}
+					information={error.information}
+				/>
+			)}
 			<form onSubmit={onSubmitHandler} className={classes.form}>
 				<label className={classes.form__heading}>Username</label>
 				<input className={classes.form__input} type='text' onChange={usernameChangeHandler} value={enteredName}></input>
 				<label className={classes.form__heading}>Age (Years)</label>
 				<input className={classes.form__input} type='number' onChange={ageChangeHandler} value={enteredAge}></input>
-				<Button type='submit'>
-					Add User
-				</Button>
-
-				{isValid ? (
-					<ErrorModal
-						isvalid={isValid}
-						onConfirm={onConfirmHandler}
-						title={'Invalid input'}
-						information={text}
-					/>
-				) : (
-					''
-				)}
+				<Button type='submit'>Add User</Button>
 			</form>
 		</Card>
 	)
